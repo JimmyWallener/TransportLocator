f7e96b1998712b85a6c478475f4cdb6af8e659a3
pellelito <pelle.lofstrand@gmail.com>
2020-01-29 12:25:11 +0100

    Update getStations.java

--- src/getStations.java
+++ src/getStations.java
@@ -1,15 +1,11 @@
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.net.URL;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.xml.parsers.DocumentBuilderFactory;
-
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
