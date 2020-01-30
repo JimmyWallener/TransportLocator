da0dc715b566741b3e7ab8d602081d2d535ed10b
pellelito <pelle.lofstrand@gmail.com>
2020-01-30 08:21:59 +0100

    Just editing some errors

--- src/API/getStations.java
+++ src/API/getStations.java
@@ -96,12 +96,12 @@ public class getStations extends HttpServlet {
 		out.print("<input type=\"button\" value=\"Search again\" onclick=\"window.location='index.jsp'\">");
 		out.println("	<div class=\"footer\">\r\n" + 
         		"		<p>Follow Us</p> \r\n" + 
-		"<a href="#" class="fa fa-facebook"></a>"+
-		"<a href="#" class="fa fa-twitter"></a>"+
-		"<a href="#" class="fa fa-google"></a>"+
-		"<a href="#" class="fa fa-linkedin"></a>"+
-		"<a href="#" class="fa fa-youtube"></a>"+
-		"<a href="#" class="fa fa-instagram"></a>"+
+		"<a href=\"#\" class=\"fa fa-facebook\"></a>"+
+		"<a href=\"#\" class=\"fa fa-twitter\"></a>"+
+		"<a href=\"#\" class=\"fa fa-google\"></a>"+
+		"<a href=\"#\" class=\"fa fa-linkedin\"></a>"+
+		"<a href=\"#\" class=\"fa fa-youtube\"></a>"+
+		"<a href=\"#\" class=\"fa fa-instagram\"></a>"+
 		
         		"	</div>"
         		+ "</body>\r\n" + 
